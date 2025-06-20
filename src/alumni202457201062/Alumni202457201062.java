7d515df1f537205fedfa575759aeeff5aa6e8e53
sindufha <sindufha215@gmail.com>
2025-06-20 15:32:12 +0700

    Final alumni

---
+++ src/alumni202457201062/Alumni202457201062.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni202457201062;
+
+/**
+ *
+ * @author MyBook Hype AMD
+ */
+public class Alumni202457201062 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
